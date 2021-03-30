e81b06ccbca2ebc8c0c20da332370531f57648dd
Jovino Neto <jovinone@gmail.com>
2021-03-30 00:06:28 -0300

    second commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,8 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("User is not an administrator!");
+    }
+
+    if (!user) {
+      throw new Error("User not found!");
+    }
+    return this.usersRepository.list();
   }
 }
-
 export { ListAllUsersUseCase };
